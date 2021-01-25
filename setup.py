5c8cbc8a00cc4411d8686d6c7a61599353d10c0a
Pedro Moura <moura.pedro123@gmail.com>
2021-01-25 18:07:55 -0300

    created setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requires = f.read().splitlines()
+
+setup(
+    name="hiddenpower",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'hiddenpower=hiddenpower.main:main'
+        ]
+    }
+)
