fb0b15ae2e636ef86bfb74edeef1486b4189d2a7
Pedro Moura <moura.pedro123@gmail.com>
2021-02-03 22:05:10 -0300

    added test cases for caesars cipher

--- tests/test_caesar.py
+++ tests/test_caesar.py
@@ -1,28 +1,57 @@
 import unittest
-import hiddenpower.commands.caesar
+from string import ascii_uppercase, ascii_lowercase, digits, punctuation, whitespace
+from hiddenpower.commands.caesar import encrypt_content, decrypt_content
 
-# TODO test argument parsing
 
-# TODO test encrypt rotation uppercase -> k=13
-#   "ZEKROM, USE BOLT STRIKE!"
-#   "MRXEBZ, HFR OBYG FGEVXR!" 
+class CaesarTest(unittest.TestCase):
+    # TODO test argument parsing
 
-# TODO test encrypt rotation lowercase -> k=13
-#   "zekrom, use bolt strike!"
-#   "mrxebz, hfr obyg fgevxr!"
+    def test_encrypt_rotation_uppercase_13(self):
+        """test encrypt rotation uppercase -> k=13"""
+        self.assertEqual(encrypt_content(ascii_uppercase), "NOPQRSTUVWXYZABCDEFGHIJKLM")
 
-# TODO test decrypt rotation uppercase -> k=13
-#   "MRXEBZ, HFR OBYG FGEVXR!"
-#   "ZEKROM, USE BOLT STRIKE!" 
+    def test_encrypt_rotation_lowercase_13(self):
+        """test encrypt rotation lowercase -> k=13"""
+        self.assertEqual(encrypt_content(ascii_lowercase), "nopqrstuvwxyzabcdefghijklm")
 
-# TODO test decrypt rotation lowercase -> k=13
-#   "mrxebz, hfr obyg fgevxr!"
-#   "zekrom, use bolt strike!"
+    def test_decrypt_rotation_uppercase_13(self):
+        """test decrypt rotation uppercase -> k=13"""
+        self.assertEqual(decrypt_content("NOPQRSTUVWXYZABCDEFGHIJKLM"), ascii_uppercase)
 
-# TODO test encrypt/decrypt special chars like é, à, õ, ã, ç, etc... -> k=13
-#   "Pegá-los eu tentarei! Vai ser grande a emoção! Pokémon!"
-#   "Crtá-ybf rh gragnerv! Inv fre tenaqr n rzbçãb! Cbxézba!"
+    def test_decrypt_rotation_lowercase_13(self):
+        """test decrypt rotation lowercase -> k=13"""
+        self.assertEqual(decrypt_content("nopqrstuvwxyzabcdefghijklm"), ascii_lowercase)
 
-# TODO test key > 25
+    def test_encrypt_non_ascii_letters_13(self):
+        """test encrypt special chars like é, à, õ, ã, ç, etc... -> k=13"""
+        content = punctuation + digits + whitespace + "àèáéíóúàãõâêîôûñ"
+        self.assertEqual(encrypt_content(content), content)
 
-# TODO test key < 0
+    def test_decrypt_non_ascii_letters_13(self):
+        """test decrypt special chars like é, à, õ, ã, ç, etc... -> k=13"""
+        content = punctuation + digits + whitespace + "àèáéíóúàãõâêîôûñ"
+        self.assertEqual(decrypt_content(content), content)
+
+    def test_encrypt_key_gt_25(self):
+        """test encrypt key > 25"""
+        self.assertEqual(
+            encrypt_content(ascii_uppercase, key=29), "DEFGHIJKLMNOPQRSTUVWXYZABC"
+        )
+
+    def test_decrypt_key_gt_25(self):
+        """test decrypt key > 25"""
+        self.assertEqual(
+            decrypt_content("DEFGHIJKLMNOPQRSTUVWXYZABC", key=29), ascii_uppercase
+        )
+
+    def test_encrypt_key_lt_0(self):
+        """test encrypt key < 25"""
+        self.assertEqual(
+            encrypt_content(ascii_lowercase, key=-3), "xyzabcdefghijklmnopqrstuvw"
+        )
+
+    def test_decrypt_key_lt_0(self):
+        """test decrypt key < 25"""
+        self.assertEqual(
+            decrypt_content("xyzabcdefghijklmnopqrstuvw", key=-3), ascii_lowercase
+        )
